ea8b41c8bd60c0573a91aafb055bdc2508f4e87c
Qandle <unmistakework@hotmail.com>
2021-05-28 02:19:27 +0700

    Add Exercise4_Chanidapa_C.py

---
+++ Exercise4_Chanidapa_C.py
@@ -0,0 +1,30 @@
+'''
+"หากผู้เรียนกำลังศึกษาอยู่ในชั้นปีที่ 2 มหาวิทยาลัยชื่อดังย่านกลางเมืองที่ตอนนี้กำลังสอบเก็บคะแนนกันอย่างสนุกสนาน
+แล้ววันนึงได้รับมอบหมายจากอาจารย์ที่ปรึกษาให้พัฒนาโปรแกรมให้กับทางคณะ
+โดย ระบบดังกล่าวจะมีการเก็บคะแนนรายบุคคลของผู้เรียนแต่ละท่าน ของนักศึกษาชั้นปีที่ 2 เทอม 1" โดยมีรายชื่อวิชาดังนี้
+
+Foundation English
+General Business
+Introduction to Computer Systems
+Computer Programming
+โดยให้ผู้เรียนพัฒนาโปรแกรมโดยสร้างตัวแปรสำหรับเก็บคะแนนผู้เรียนในรายวิชาดังกล่าว
+โดยคะแนนจะสามารถเป็นตัวเลขทศนิยมได้ และ เมื่อได้ตัวแปรมาแล้วให้ทำการแสดงผลออกมาให้ผู้ใช้งานได้เห็นในรูปแบบ
+
+--- Your Score ---
+Foundation English : คะแนนที่ได้
+General Business : คะแนนที่ได้
+Introduction to Computer Systems : คะแนนที่ได้
+Computer Programming : คะแนนที่ได้
+
+*การตั้งชื่อตัวแปรควรสื่อความหมาย ผู้อ่าน หรือ นักพัฒนาโปรแกรมท่านอื่นสามารถอ่านเข้าใจได้
+'''
+
+foundationEnglish = 80.4
+generalBusiness = 49.8
+introductionToComputerSystems = 50.9
+computerProgramming = 90.0
+print("--- Your Score ---")
+print("Foundation English :", foundationEnglish)
+print("General Business :", generalBusiness)
+print("Introduction to Computer Systems :", introductionToComputerSystems)
+print("Computer Programming :", computerProgramming)
