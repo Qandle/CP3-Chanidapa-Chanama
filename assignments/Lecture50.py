16e2713b64aa291fd3a5c8ab17bfa11473b3da4b
Qandle <unmistakework@hotmail.com>
2021-06-17 00:19:35 +0700

    Create Lecture50.py

---
+++ assignments/Lecture50.py
@@ -0,0 +1,23 @@
+def add_number(x, y):
+    print(x+y)
+
+
+def minus_number(x, y):
+    print(x-y)
+
+
+def time_number(x, y):
+    print(x*y)
+
+
+def divide_number(x, y):
+    if y == 0:
+        print("error")
+    else:
+        print(x/y)
+
+
+add_number(2, 5)
+minus_number(9, 4)
+time_number(6, 5)
+divide_number(7, 9)
